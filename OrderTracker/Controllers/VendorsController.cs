78187a08be70dc8e6f910b647899a206db6c1aec
Rafael Petrachini <raffp@me.com>
2022-12-10 20:21:30 -0800

    add README file

--- OrderTracker/Controllers/VendorsController.cs
+++ OrderTracker/Controllers/VendorsController.cs
@@ -53,9 +53,3 @@ namespace ForSale.Controllers
   }
 
 }
-
-
-//I think the quality and efficiency of the material fell sharply after c# course started. it is a lot more complex than JS but we are moving with the same pace, if not faster. We are hardly processing new info before 20 lessons of reading is thrown at us for the next sunday. 
-
-//Also, the big reading lessons are always coinciding with code reviews week, so form thursday to sunday we are expected to do a code review (minimum 4 hours put in, always takes a lot more) + 15 pages of readings, 
-//resultings in at least 35h/week when we signed up for 25. It just leads to frustration to those who work full time in a week. (40h of work a week plus 8h of sleep + 25h of epicodus = 120h of the week (168h). That leaves us with 48h/week for everything else, on top of code view + reading (10h). It becomes impossible. It almost feels like it was an impossible promise of learning all of this in a xnumber of weeks and smooching the material in that x number of weeks when it could be stretched out by a month or two in a more doable way without overloading and stressing out students with a full time job. we are already flying through allof this material so quickly that it even feels like we are not really learning, so there's no reason to be so fast. resulting in drop outs. what is the benefit of doing it so quickly if we dont be actually processing and learning?
---
+++ README.md
@@ -0,0 +1,30 @@
+# (Bakery Project)
+
+#### (A C# program for a bakery that allows the owner to create a database of his vendors and their orders. )
+
+
+#### By (Rafael Petrachini)
+
+## Technologies Used
+
+* _List all_
+* _the major technologies_
+* _you used in your project_
+* _here_
+
+## Description
+
+## Setup/Installation Requirements
+
+* _This is a great place_
+* _to list setup instructions_
+* _in a simple_
+* _easy-to-understand_
+* _format_
+
+## Known Bugs
+
+* _Any known issues_
+* _should go here_
+
+## License
